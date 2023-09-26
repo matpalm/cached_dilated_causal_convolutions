c939254f5f42dc0e22d327024864652d45fa5ebc
Mat Kelcey <matthew.kelcey@gmail.com>
2023-09-27 08:09:00 +1000

    more equivlance tests

--- fxpmath_version/test_qkeras_fxpmath_equivlance.py
+++ fxpmath_version/test_qkeras_fxpmath_equivlance.py
@@ -10,39 +10,54 @@ from qkeras import *
 
 from fxpmath import Fxp
 
-# Some utils
+# a number of tests that train a qkeras model and then replicate the basic
+# inference using only fxpmath operations. we do this as a fpga prototype
+# because we will be able to implement the fxpmath versions exactly in
+# verilog
+
+np.set_printoptions(precision=32)
 
 N_WORD = 16
 N_INT = 4
 N_FRAC = 12
 assert N_WORD == N_INT + N_FRAC
 
+# convert a value to the target fixed point representation for
+# values or weights
 def single_width_fxp(v):
   return Fxp(v, signed=True, n_word=N_WORD, n_frac=N_FRAC)
 
+# convert a value to the double width target fixed point
+# representation that will be used for products and accumulators
 def double_width_fxp(v):
   return Fxp(v, signed=True, n_word=N_WORD*2, n_frac=N_FRAC*2)
 
+def bits(fxp):
+    return fxp.bin(frac_dot=True)
+
 # util to convert numpy array X to float values in QI.F
-def to_fixed_point(x):
-  return float(Fxp(x, signed=True, n_word=N_WORD, n_frac=N_FRAC))
-to_fixed_point = np.vectorize(to_fixed_point)
+def array_to_fixed_point(x):
+  return float(single_width_fxp(x))
+array_to_fixed_point = np.vectorize(array_to_fixed_point)
 
 # check all values in array are QI.F
 def check_all_qIF(a):
   for v in a.flatten():
-    q_val = float(Fxp(v, signed=True, n_word=N_WORD, n_frac=N_FRAC))
+    q_val = float(single_width_fxp(v))
     if v != q_val:
       raise Exception(f"value {v} not representable in QI.F; it converted to {q_val}")
 
 # debug util to convert a numpy array X to their QI.F binary string form
-def to_fixed_point_bin_str(x):
-  return Fxp(x, signed=True, n_word=N_WORD, n_frac=N_FRAC).bin(frac_dot=True)
-to_fixed_point_bin_str = np.vectorize(to_fixed_point_bin_str)
+def array_to_fixed_point_bin_str(x):
+  return bits(single_width_fxp(x))
+array_to_fixed_point_bin_str = np.vectorize(array_to_fixed_point_bin_str)
 
-# qkeras quantiser for kernel and biases
+
+
+
+# qkeras quantiser for all tests weights, kernels and biases
 def quantiser():
-    return quantized_bits(bits=N_WORD-1, integer=N_INT, alpha=1)
+    return quantized_bits(bits=N_WORD, integer=N_INT, alpha=1)
 
 
 class TestQKerasFxpMathEquivalance(unittest.TestCase):
@@ -109,9 +124,9 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
         test_y = np.empty((N, OUT_D), dtype=float)
         test_y[:,0] = (test_x[:,0] + test_x[:,1])/2
         test_y[:,1] = -(test_x[:,0] + test_x[:,2])/2
-        # convert them to Q2.16
-        test_x = to_fixed_point(test_x)
-        test_y = to_fixed_point(test_y)
+        # convert them to fixed point
+        test_x = array_to_fixed_point(test_x)
+        test_y = array_to_fixed_point(test_y)
 
         # train model. given a reasonable number of examples this model should be near perfect
         h = single_layer_model.fit(test_x, test_y, epochs=300, verbose=False)
@@ -123,7 +138,7 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
         quantised_weights = qkeras.utils.model_save_quantized_weights(single_layer_model)
         layer0_q_weights = quantised_weights['single_dense']['weights'][0]
         layer0_q_biases = quantised_weights['single_dense']['weights'][1]
-        # check they are all representable by Q2.16
+        # check they are all representable fixed point
         check_all_qIF(layer0_q_weights)
         check_all_qIF(layer0_q_biases)
 
@@ -173,6 +188,12 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
 
     def test_underfit_qkeras_dense_and_custom_fxpmath_inference(self):
 
+        # same model as above but with random data. the model will not
+        # be able to fit this exactly so we expect weights to be generated
+        # that are not all zeros on most significant 1/N bits
+        # recall; call qkeras model(x) uses the FULL floats for the calculations
+        # not the quantised weights
+
         IN_D = 3
         OUT_D = 2
         inp = Input((IN_D,))
@@ -194,8 +215,8 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
         test_y = (np.random.uniform(size=(N, OUT_D))*2)-1  #  (-1, 1)
         test_y *= 1.9
         # convert them to Q2.16
-        test_x = to_fixed_point(test_x)
-        test_y = to_fixed_point(test_y)
+        test_x = array_to_fixed_point(test_x)
+        test_y = array_to_fixed_point(test_y)
 
         # train model. because of random data and small model we expect this to be
         # no where near perfect
@@ -214,8 +235,10 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
         # extract weights
         col0_weights = layer0_q_weights[:,0]
         col0_bias = layer0_q_biases[0]
+        #print("col0_weights/biases", list(map(float, col0_weights)), float(col0_bias))
         col1_weights = layer0_q_weights[:,1]
         col1_bias = layer0_q_biases[1]
+        #print("col1_weights/biases", list(map(float, col1_weights)), float(col1_bias))
 
         # create simple dot product with bias method
         def dot_product_with_bias(x, weights, bias):
@@ -223,14 +246,21 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
             # init accumulator with bias value, in double width ready to be accumulated
             # with dot product results
             accum = double_width_fxp(bias)
+            #print("accum inited with bias [", bias,"] as ", bits(accum))
             # this loop represents what will be in the state machine
             for i in range(len(x)):
                 x_i = single_width_fxp(x[i])
                 w_i = single_width_fxp(weights[i])
-                prod = x_i * w_i  # recall; product will be double width
-                accum += prod
+                product = x_i * w_i  # recall; product will be double width
+                accum += product
+                # keep accumulator single width. by dft a+b => +1 for int part
+                accum.resize(signed=True, n_word=2*N_WORD, n_frac=2*N_FRAC)
+                #print("i", i, "x_i", bits(x_i), "w_i", bits(w_i), "=> product", bits(product), "=> accum", bits(accum), accum)
+
             # return result resized down to single width
+            #print("accum (pre resize)  ", bits(accum), accum)
             accum.resize(signed=True, n_word=N_WORD, n_frac=N_FRAC)
+            #print("accum (post resize) ", bits(accum), accum)
             return accum
 
         # predict for single example as explicit dot product for each of
@@ -251,6 +281,10 @@ class TestQKerasFxpMathEquivalance(unittest.TestCase):
         test_y_pred = predict_set(test_x)
         custom_mse = np.mean((test_y_pred-test_y) ** 2)
         print("custom_mse", custom_mse)
+
+        mse_diff = abs(custom_mse-single_layer_model_eval_score)
+        print("mse diff", mse_diff)
+        self.assertTrue(mse_diff < 1e-3)
 
 if __name__ == '__main__':
     unittest.main()
