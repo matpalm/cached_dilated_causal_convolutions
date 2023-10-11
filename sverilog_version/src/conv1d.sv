640283748c4a5aaa377efa3bd72a05feab5087e2
Mat Kelcey <matthew.kelcey@gmail.com>
2023-10-11 16:30:20 +1100

    make conv1d 8x8

--- sverilog_version/src/conv1d.sv
+++ sverilog_version/src/conv1d.sv
@@ -7,26 +7,52 @@ module conv1d #(
   input                        clk,
   input                        rst,
   input                        apply_relu,
+
   input signed [W-1:0]         a0_d0,
   input signed [W-1:0]         a0_d1,
   input signed [W-1:0]         a0_d2,
   input signed [W-1:0]         a0_d3,
+  input signed [W-1:0]         a0_d4,
+  input signed [W-1:0]         a0_d5,
+  input signed [W-1:0]         a0_d6,
+  input signed [W-1:0]         a0_d7,
+
   input signed [W-1:0]         a1_d0,
   input signed [W-1:0]         a1_d1,
   input signed [W-1:0]         a1_d2,
   input signed [W-1:0]         a1_d3,
+  input signed [W-1:0]         a1_d4,
+  input signed [W-1:0]         a1_d5,
+  input signed [W-1:0]         a1_d6,
+  input signed [W-1:0]         a1_d7,
+
   input signed [W-1:0]         a2_d0,
   input signed [W-1:0]         a2_d1,
   input signed [W-1:0]         a2_d2,
   input signed [W-1:0]         a2_d3,
+  input signed [W-1:0]         a2_d4,
+  input signed [W-1:0]         a2_d5,
+  input signed [W-1:0]         a2_d6,
+  input signed [W-1:0]         a2_d7,
+
   input signed [W-1:0]         a3_d0,
   input signed [W-1:0]         a3_d1,
   input signed [W-1:0]         a3_d2,
   input signed [W-1:0]         a3_d3,
+  input signed [W-1:0]         a3_d4,
+  input signed [W-1:0]         a3_d5,
+  input signed [W-1:0]         a3_d6,
+  input signed [W-1:0]         a3_d7,
+
   output reg signed [W-1:0]    out_d0,
   output reg signed [W-1:0]    out_d1,
   output reg signed [W-1:0]    out_d2,
   output reg signed [W-1:0]    out_d3,
+  output reg signed [W-1:0]    out_d4,
+  output reg signed [W-1:0]    out_d5,
+  output reg signed [W-1:0]    out_d6,
+  output reg signed [W-1:0]    out_d7,
+
   output reg                   out_v
 );
 
@@ -45,50 +71,58 @@ module conv1d #(
 
     // for whatever reason these don't have a valid value (just xxx ) during accumulation
     // but _can_ access kernel0.out (?)
-    reg signed [2*W-1:0]  kernel0_out [0:3];
-    reg signed [2*W-1:0]  kernel1_out [0:3];
-    reg signed [2*W-1:0]  kernel2_out [0:3];
-    reg signed [2*W-1:0]  kernel3_out [0:3];
+    reg signed [2*W-1:0]  kernel0_out [0:7];
+    reg signed [2*W-1:0]  kernel1_out [0:7];
+    reg signed [2*W-1:0]  kernel2_out [0:7];
+    reg signed [2*W-1:0]  kernel3_out [0:7];
 
     // double width accumulator
-    reg signed [2*W-1:0]  accum [0:3];
+    reg signed [2*W-1:0]  accum [0:7];
 
     // single width final result
-    reg signed [W-1:0]  result [0:3];
+    reg signed [W-1:0]  result [0:7];
 
     // bias values
     initial begin
         $readmemh({B_VALUES,"/bias.hex"}, bias_values);
     end
-    reg signed [2*W-1:0] bias_values [0:3];
+    reg signed [2*W-1:0] bias_values [0:7];
 
     // 4 kernel mat muls
 
     row_by_matrix_multiply #(.B_VALUES({B_VALUES,"/k0"})) kernel0 (
         .clk(clk), .rst(rst),
         .a_d0(a0_d0), .a_d1(a0_d1), .a_d2(a0_d2), .a_d3(a0_d3),
+        .a_d4(a0_d4), .a_d5(a0_d5), .a_d6(a0_d6), .a_d7(a0_d7),
         .out_d0(kernel0_out[0]), .out_d1(kernel0_out[1]), .out_d2(kernel0_out[2]), .out_d3(kernel0_out[3]),
+        .out_d4(kernel0_out[4]), .out_d5(kernel0_out[5]), .out_d6(kernel0_out[6]), .out_d7(kernel0_out[7]),
         .out_v(kernel0_v)
     );
 
     row_by_matrix_multiply #(.B_VALUES({B_VALUES,"/k1"})) kernel1 (
         .clk(clk), .rst(rst),
         .a_d0(a1_d0), .a_d1(a1_d1), .a_d2(a1_d2), .a_d3(a1_d3),
+        .a_d4(a1_d4), .a_d5(a1_d5), .a_d6(a1_d6), .a_d7(a1_d7),
         .out_d0(kernel1_out[0]), .out_d1(kernel1_out[1]), .out_d2(kernel1_out[2]), .out_d3(kernel1_out[3]),
+        .out_d4(kernel1_out[4]), .out_d5(kernel1_out[5]), .out_d6(kernel1_out[6]), .out_d7(kernel1_out[7]),
         .out_v(kernel1_v)
     );
 
     row_by_matrix_multiply #(.B_VALUES({B_VALUES,"/k2"})) kernel2 (
         .clk(clk), .rst(rst),
         .a_d0(a2_d0), .a_d1(a2_d1), .a_d2(a2_d2), .a_d3(a2_d3),
+        .a_d4(a2_d4), .a_d5(a2_d5), .a_d6(a2_d6), .a_d7(a2_d7),
         .out_d0(kernel2_out[0]), .out_d1(kernel2_out[1]), .out_d2(kernel2_out[2]), .out_d3(kernel2_out[3]),
+        .out_d4(kernel2_out[4]), .out_d5(kernel2_out[5]), .out_d6(kernel2_out[6]), .out_d7(kernel2_out[7]),
         .out_v(kernel2_v)
     );
 
     row_by_matrix_multiply #(.B_VALUES({B_VALUES,"/k3"})) kernel3 (
         .clk(clk), .rst(rst),
         .a_d0(a3_d0), .a_d1(a3_d1), .a_d2(a3_d2), .a_d3(a3_d3),
+        .a_d4(a3_d4), .a_d5(a3_d5), .a_d6(a3_d6), .a_d7(a3_d7),
         .out_d0(kernel3_out[0]), .out_d1(kernel3_out[1]), .out_d2(kernel3_out[2]), .out_d3(kernel3_out[3]),
+        .out_d4(kernel3_out[4]), .out_d5(kernel3_out[5]), .out_d6(kernel3_out[6]), .out_d7(kernel3_out[7]),
         .out_v(kernel3_v)
     );
 
@@ -108,6 +142,10 @@ module conv1d #(
                     accum[1] <= kernel0_out[1] + kernel1_out[1] + kernel2_out[1] + kernel3_out[1];
                     accum[2] <= kernel0_out[2] + kernel1_out[2] + kernel2_out[2] + kernel3_out[2];
                     accum[3] <= kernel0_out[3] + kernel1_out[3] + kernel2_out[3] + kernel3_out[3];
+                    accum[4] <= kernel0_out[4] + kernel1_out[4] + kernel2_out[4] + kernel3_out[4];
+                    accum[5] <= kernel0_out[5] + kernel1_out[5] + kernel2_out[5] + kernel3_out[5];
+                    accum[6] <= kernel0_out[6] + kernel1_out[6] + kernel2_out[6] + kernel3_out[6];
+                    accum[7] <= kernel0_out[7] + kernel1_out[7] + kernel2_out[7] + kernel3_out[7];
                     c1d_state <= BIAS_ADD;
                 end
                 BIAS_ADD: begin
@@ -115,6 +153,10 @@ module conv1d #(
                     accum[1] <= accum[1] + bias_values[1];
                     accum[2] <= accum[2] + bias_values[2];
                     accum[3] <= accum[3] + bias_values[3];
+                    accum[4] <= accum[4] + bias_values[4];
+                    accum[5] <= accum[5] + bias_values[5];
+                    accum[6] <= accum[6] + bias_values[6];
+                    accum[7] <= accum[7] + bias_values[7];
                     c1d_state <= SINGLE_W;
                 end
                 SINGLE_W: begin
@@ -122,6 +164,10 @@ module conv1d #(
                     result[1] <= accum[1][27:12];
                     result[2] <= accum[2][27:12];
                     result[3] <= accum[3][27:12];
+                    result[4] <= accum[4][27:12];
+                    result[5] <= accum[5][27:12];
+                    result[6] <= accum[6][27:12];
+                    result[7] <= accum[7][27:12];
                     c1d_state = OUTPUT;
                 end
                 OUTPUT: begin
@@ -129,6 +175,10 @@ module conv1d #(
                     out_d1 <= apply_relu ? `relu(result[1]) : result[1];
                     out_d2 <= apply_relu ? `relu(result[2]) : result[2];
                     out_d3 <= apply_relu ? `relu(result[3]) : result[3];
+                    out_d4 <= apply_relu ? `relu(result[4]) : result[4];
+                    out_d5 <= apply_relu ? `relu(result[5]) : result[5];
+                    out_d6 <= apply_relu ? `relu(result[6]) : result[6];
+                    out_d7 <= apply_relu ? `relu(result[7]) : result[7];
                     out_v <= 1;
                 end
             endcase
--- sverilog_version/tests/conv1d/conv1d_tb.py
+++ sverilog_version/tests/conv1d/conv1d_tb.py
@@ -25,28 +25,44 @@ async def test_conv1d(dut):
     dut.a0_d1.value = 0xFDFC   # 1111.110111111100 -0.1259765625
     dut.a0_d2.value = 0x0506   # 0000.010100000110 0.31396484375
     dut.a0_d3.value = 0xF000   # 1111.000000000000 -1.0
+    dut.a0_d4.value = 0x0400   # 0000.010000000000 0.25
+    dut.a0_d5.value = 0xFDFC   # 1111.110111111100 -0.1259765625
+    dut.a0_d6.value = 0x0506   # 0000.010100000110 0.31396484375
+    dut.a0_d7.value = 0xF000   # 1111.000000000000 -1.0
 
     dut.a1_d0.value = 0x0400   # 0000.010000000000 0.25
     dut.a1_d1.value = 0xFDFC   # 1111.110111111100 -0.1259765625
     dut.a1_d2.value = 0x0506   # 0000.010100000110 0.31396484375
     dut.a1_d3.value = 0xF000   # 1111.000000000000 -1.0
+    dut.a1_d4.value = 0x0400   # 0000.010000000000 0.25
+    dut.a1_d5.value = 0xFDFC   # 1111.110111111100 -0.1259765625
+    dut.a1_d6.value = 0x0506   # 0000.010100000110 0.31396484375
+    dut.a1_d7.value = 0xF000   # 1111.000000000000 -1.0
 
     dut.a2_d0.value = 0x0400   # 0000.010000000000 0.25
     dut.a2_d1.value = 0xFDFC   # 1111.110111111100 -0.1259765625
     dut.a2_d2.value = 0x0506   # 0000.010100000110 0.31396484375
     dut.a2_d3.value = 0xF000   # 1111.000000000000 -1.0
+    dut.a2_d4.value = 0x0400   # 0000.010000000000 0.25
+    dut.a2_d5.value = 0xFDFC   # 1111.110111111100 -0.1259765625
+    dut.a2_d6.value = 0x0506   # 0000.010100000110 0.31396484375
+    dut.a2_d7.value = 0xF000   # 1111.000000000000 -1.0
 
     dut.a3_d0.value = 0x0400   # 0000.010000000000 0.25
     dut.a3_d1.value = 0xFDFC   # 1111.110111111100 -0.1259765625
     dut.a3_d2.value = 0x0506   # 0000.010100000110 0.31396484375
     dut.a3_d3.value = 0xF000   # 1111.000000000000 -1.0
+    dut.a3_d4.value = 0x0400   # 0000.010000000000 0.25
+    dut.a3_d5.value = 0xFDFC   # 1111.110111111100 -0.1259765625
+    dut.a3_d6.value = 0x0506   # 0000.010100000110 0.31396484375
+    dut.a3_d7.value = 0xF000   # 1111.000000000000 -1.0
 
     dut.rst.value = 1
     await RisingEdge(dut.clk)
     dut.rst.value = 0
     await RisingEdge(dut.clk)
 
-    for i in range(10):
+    for i in range(30):
         print("i", i, "c1d_state", dut.c1d_state.value)
         print("accum      ", dut.accum.value)
         print("result     ", dut.result.value)
@@ -55,10 +71,14 @@ async def test_conv1d(dut):
         await RisingEdge(dut.clk)
 
     assert dut.out_v.value == 1
-    assert dut.out_d0.value == 0xFE7E  # 1111 1110 0111 1110
-    assert dut.out_d1.value == 0xF716  # 1111 0111 0001 0110
-    assert dut.out_d2.value == 0x0326  # 0000 0011 0010 0110
-    assert dut.out_d3.value == 0x0E61  # 0000 1110 0110 0001
+    assert dut.out_d0.value == 0b0001011011100101
+    assert dut.out_d1.value == 0b1111111010111111
+    assert dut.out_d2.value == 0b0001011001000010
+    assert dut.out_d3.value == 0b1111100011101001
+    assert dut.out_d4.value == 0b0000101110100011
+    assert dut.out_d5.value == 0b0001110010101001
+    assert dut.out_d6.value == 0b0000011101101110
+    assert dut.out_d7.value == 0b0001000111000011
 
     # same again with RELU
 
@@ -69,14 +89,17 @@ async def test_conv1d(dut):
     dut.rst.value = 0
     await RisingEdge(dut.clk)
 
-    for i in range(10):
+    for i in range(30):
         if dut.out_v.value:
             break
         await RisingEdge(dut.clk)
 
     assert dut.out_v.value == 1
-
-    assert dut.out_d0.value == 0
+    assert dut.out_d0.value == 0b0001011011100101
     assert dut.out_d1.value == 0
-    assert dut.out_d2.value == 0x0326  # 0000 0011 0010 0110
-    assert dut.out_d3.value == 0x0E61  # 0000 1110 0110 0001
+    assert dut.out_d2.value == 0b0001011001000010
+    assert dut.out_d3.value == 0
+    assert dut.out_d4.value == 0b0000101110100011
+    assert dut.out_d5.value == 0b0001110010101001
+    assert dut.out_d6.value == 0b0000011101101110
+    assert dut.out_d7.value == 0b0001000111000011
