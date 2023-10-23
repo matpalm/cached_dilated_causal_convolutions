5bfb989b01da0c0622e5b91f3e0fec931637ada3
Mat Kelcey <matthew.kelcey@gmail.com>
2023-10-23 21:21:18 +1100

    do elements of dot product in single state

--- sverilog_version/src/dot_product.sv
+++ sverilog_version/src/dot_product.sv
@@ -17,27 +17,18 @@ module dot_product #(
 
     // unpack a
     logic signed [W-1:0] a[D];
-    genvar i;
+    genvar j;
     generate
-        for (i = 0; i < D; i++) begin
-            assign a[i] = packed_a[W*(D-i-1) +: W];
+        for (j=0; j<D; j++) begin
+            assign a[j] = packed_a[W*(D-j-1) +: W];
         end
     endgenerate
 
-    // state machine for pipelined mulitplies ( in pairs )
-    // and accumulation.
     localparam
-        MULT_D0     = 0,
-        MULT_D1     = 1,
-        MULT_D2     = 2,
-        MULT_D3     = 3,
-        MULT_D4     = 4,
-        MULT_D5     = 5,
-        MULT_D6     = 6,
-        MULT_D7     = 7,
-        FINAL_ADD   = 8,
-        DONE        = 9;
-    reg [3:0] dp_state = MULT_D0;
+        MULTIPLY_ELEMENT     = 0,
+        FINAL_ADD            = 1,
+        DONE                 = 2;
+    reg [2:0] dp_state = MULTIPLY_ELEMENT;
 
     // see https://projectf.io/posts/fixed-point-numbers-in-verilog/
     reg signed [2*W-1:0] acc0;
@@ -53,54 +44,22 @@ module dot_product #(
     end
     reg signed [W-1:0] b_values [0:D-1];
 
+    integer i;
+
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            dp_state <= MULT_D0;
+            acc0 <= 0;
+            product0 <= 0;
+            i <= 0;
+            dp_state <= MULTIPLY_ELEMENT;
             out_v <= 0;
         end else
             case(dp_state)
-                // TODO: with a an array too we can fold all these
-                // into one "state" with an i value; a[i] * b[i]
-                MULT_D0: begin
-                    acc0 <= 0;
-                    product0 <= a[0] * b_values[0];
-                    dp_state <= MULT_D1;
-                end
-                MULT_D1: begin
+                MULTIPLY_ELEMENT: begin
                     acc0 <= acc0 + product0;
-                    product0 <= a[1] * b_values[1];
-                    dp_state <= MULT_D2;
-                end
-                MULT_D2: begin
-                    acc0 <= acc0 + product0;
-                    product0 <= a[2] * b_values[2];
-                    dp_state <= MULT_D3;
-                end
-                MULT_D3: begin
-                    acc0 <= acc0 + product0;
-                    product0 <= a[3] * b_values[3];
-                    dp_state <= MULT_D4;
-                    // TODO could shortcut here for 4D; dp_state <= D==4 ? FINAL_ADD : MULT_D4;
-                end
-                MULT_D4: begin
-                    acc0 <= acc0 + product0;
-                    product0 <= a[4] * b_values[4];
-                    dp_state <= MULT_D5;
-                end
-                MULT_D5: begin
-                    acc0 <= acc0 + product0;
-                    product0 <= a[5] * b_values[5];
-                    dp_state <= MULT_D6;
-                end
-                MULT_D6: begin
-                    acc0 <= acc0 + product0;
-                    product0 <= a[6] * b_values[6];
-                    dp_state <= MULT_D7;
-                end
-                MULT_D7: begin
-                    acc0 <= acc0 + product0;
-                    product0 <= a[7] * b_values[7];
-                    dp_state <= FINAL_ADD;
+                    product0 <= a[i] * b_values[i];
+                    i <= i + 1;
+                    dp_state <= (i == D-1) ? FINAL_ADD : MULTIPLY_ELEMENT;
                 end
                 FINAL_ADD: begin
                     acc0 <= acc0 + product0;
