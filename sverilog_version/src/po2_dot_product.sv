aab37a26056f1ed4c16a07119b2689b5fe2dffa7
Mat Kelcey <matthew.kelcey@gmail.com>
2023-11-09 22:44:44 +1100

    generalise code a bit

--- sverilog_version/src/po2_dot_product.sv
+++ sverilog_version/src/po2_dot_product.sv
@@ -27,8 +27,9 @@ module po2_dot_product #(
         START_NEXT_MULT  = 0,
         WAIT_FOR_MULT    = 1,
         ACCUMULATE       = 2,
-        DONE             = 3;
-    reg [2:0] state;
+        FINAL_ACCUMULATE = 3,
+        DONE             = 4;
+    reg [3:0] state;
 
     // D bits representing whether the Dth weight is zero or negative
     reg zero_weights [0:D-1];
@@ -46,17 +47,19 @@ module po2_dot_product #(
         out_v <= 0;
     end
 
+    localparam N_MULTS = 2;
+
     // accumulator for all po2_multiply results
-    reg signed [2*W-1:0] accumulator;
+    reg signed [2*W-1:0] accumulator [0:N_MULTS-1];
 
     // registers to handle in/out of po2 mult
     reg                  po2_rst;
-    reg signed [W-1:0]   po2_inp [0:1];
-    reg                  po2_zero_weight [0:1];
-    reg                  po2_negative_weight [0:1];
-    reg [W-1:0]          po2_log_2_weight [0:1];
-    reg signed [2*W-1:0] po2_result [0:1];
-    reg [1:0]            po2_result_v;
+    reg signed [W-1:0]   po2_inp [0:N_MULTS-1];
+    reg                  po2_zero_weight [0:N_MULTS-1];
+    reg                  po2_negative_weight [0:N_MULTS-1];
+    reg [W-1:0]          po2_log_2_weight [0:N_MULTS-1];
+    reg signed [2*W-1:0] po2_result [0:N_MULTS-1];
+    reg [N_MULTS-1:0]    po2_result_v;
 
     po2_multiply #(.W(W), .I(4)) po2_mult_0 (
         .clk(clk), .rst(po2_rst), .inp(po2_inp[0]),
@@ -73,10 +76,13 @@ module po2_dot_product #(
     );
 
     integer i;
+    integer k;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            accumulator <= 0;
+            for (k=0; k<N_MULTS; k=k+1) begin
+                accumulator[k] <= 0;
+            end
             i <= 0;
             state <= START_NEXT_MULT;
             out_v <= 0;
@@ -84,14 +90,12 @@ module po2_dot_product #(
             case(state)
                 START_NEXT_MULT: begin
                     po2_rst <= 1;
-                    po2_inp[0] <= a[i];
-                    po2_inp[1] <= a[i+1];
-                    po2_zero_weight[0] <= zero_weights[i];
-                    po2_zero_weight[1] <= zero_weights[i+1];
-                    po2_negative_weight[0] <= negative_weights[i];
-                    po2_negative_weight[1] <= negative_weights[i+1];
-                    po2_log_2_weight[0] <= log_2_weights[i];
-                    po2_log_2_weight[1] <= log_2_weights[i+1];
+                    for (k=0; k<N_MULTS; k=k+1) begin
+                        po2_inp[k] <= a[i+k];
+                        po2_zero_weight[k] <= zero_weights[i+k];
+                        po2_negative_weight[k] <= negative_weights[i+k];
+                        po2_log_2_weight[k] <= log_2_weights[i+k];
+                    end
                     state <= WAIT_FOR_MULT;
                 end
                 WAIT_FOR_MULT: begin
@@ -99,12 +103,18 @@ module po2_dot_product #(
                     if (po2_result_v == '1) state <= ACCUMULATE;
                 end
                 ACCUMULATE: begin
-                    accumulator <= accumulator + po2_result[0] + po2_result[1];
-                    i <= i + 2;
-                    state <= (i == D-2) ? DONE : START_NEXT_MULT;  // -2 for 2 dot products
+                    for (k=0; k<N_MULTS; k=k+1) begin
+                        accumulator[k] <= accumulator[k] + po2_result[k];
+                    end
+                    i <= i + N_MULTS;
+                    state <= (i == D-N_MULTS) ? FINAL_ACCUMULATE : START_NEXT_MULT;  // -N_MULTS for N_MULTS dot products
+                end
+                FINAL_ACCUMULATE: begin
+                    accumulator[0] <= accumulator[0] + accumulator[1]; // + accumulator[2] + accumulator[3];
+                    state <= DONE;
                 end
                 DONE: begin
-                    out <= accumulator;
+                    out <= accumulator[0];
                     out_v <= 1;
                 end
             endcase
