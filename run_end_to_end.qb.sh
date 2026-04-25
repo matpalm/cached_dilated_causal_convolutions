a92d5d3301b4cfb6a4139d1f54cb2241fb4fd014
Mat Kelcey <matthew.kelcey@gmail.com>
2026-04-25 10:43:55 +1000

    training and fxpmath check under uv

--- run_end_to_end.qb.sh
+++ run_end_to_end.qb.sh
@@ -1,38 +1,37 @@
 set -ex
 
-export RUN=32_qb_regression_4d
+export RUN=32_qb_regression_4d_20260425
 export DRD=datalogger_firmware/data/2d_embed_interp/wide_freq_range/24kHz
 export FILTER_D=4
 
 [ ! -d runs/$RUN ] && mkdir runs/$RUN
 
-unset CUDA_VISIBLE_DEVICES
-time python3 -m qkeras_version.train \
+time uv run -m qkeras_version.train \
  --run $RUN \
  --data-root-dir $DRD \
  --num-layers 3 --in-out-d 4 --filter-size $FILTER_D \
  --num-train-egs 20000 --epochs 5 --learning-rate 1e-3 --l2 0.0001 \
  | tee runs/$RUN/qkeras_version.train.out
 
-export CUDA_VISIBLE_DEVICES=""
-time python3 -m fxpmath_version.test \
+time uv run -m fxpmath_version.test \
  --data-root-dir $DRD \
  --load-weights runs/$RUN/weights/qkeras/latest.pkl \
  --layer-info runs/$RUN/qkeras_model.layer_info.json \
  --test-x-dir runs/$RUN/test_x_files/ \
  --plot-dir runs/$RUN/ \
  --write-verilog-weights runs/$RUN/weights/verilog/latest \
- --num-test-egs 300 \
+ --num-test-egs 500 \
  | tee runs/$RUN/fxpmath_version.test.out
-unset CUDA_VISIBLE_DEVICES
 
-pushd sverilog_version/src
-[ -f network.sv ] && rm network.sv
-ln -s qb_network.sv network.sv
-popd
+echo "VERILOG VERSION DOESNT WORK IN EXISTING UV ENV"
 
-# note: make files use FILTER_D
-WAVE=sine ./run_make_network.sh
-WAVE=ramp ./run_make_network.sh
-WAVE=square ./run_make_network.sh
-WAVE=zigzag ./run_make_network.sh
+# pushd sverilog_version/src
+# [ -f network.sv ] && rm network.sv
+# ln -s qb_network.sv network.sv
+# popd
+
+# # note: make files use FILTER_D
+# WAVE=sine ./run_make_network.sh
+# WAVE=ramp ./run_make_network.sh
+# WAVE=square ./run_make_network.sh
+# WAVE=zigzag ./run_make_network.sh
