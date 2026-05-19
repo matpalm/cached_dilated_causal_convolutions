ee487fe2196604c1c4cbe1d9e37ae57e619b7c3a
Mat Kelcey <matthew.kelcey@gmail.com>
2026-05-19 20:10:12 +1000

    wip; trains ok, and tb callback looks good, but fails on tiliqua need to double check amaranth sim ?

--- run_end_to_end.sh
+++ run_end_to_end.sh
@@ -1,23 +1,22 @@
 set -ex
 
-# qkeras 0.9.0 is not compatible with Keras 3 APIs used by default in TF 2.16+
-# so force TensorFlow to use the legacy tf.keras (tf_keras package).
+# qkeras 0.9.0 not compatible with keras from in tf 2.16; force legacy package
 export TF_USE_LEGACY_KERAS=1
 
-export RUN=106_8_8_quadrature
-export FILTERS="8 8"
+export RUN=130_8_16_8_quadrature2
+export FILTERS="8 16 8"
 
 mkdir runs/$RUN || true
 
 # pre train at FP4.12
-# time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.train \
-#  --run $RUN \
-#  --min-note A4 --max-note A4 \
-#  --fp-int 4 --fp-frac 12 \
-#  --in-out-d 4 --filter-sizes $FILTERS \
-#  --alpha-mse 1.0 --beta-stft 0.0 --beta-stft-ramp-epochs 20 \
-#  --num-train-egs 10000 --epochs 10 --learning-rate 1e-3 --l2 0.0001 \
-#  | tee runs/$RUN/qkeras_version.train.out
+time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.train \
+ --run $RUN \
+ --min-note A3 --max-note A5 \
+ --fp-int 4 --fp-frac 12 \
+ --in-out-d 4 --filter-sizes $FILTERS \
+ --alpha-mse 1.0 --beta-stft 0.1 --beta-stft-ramp-epochs 10 \
+ --num-train-egs 40000 --epochs 60 --learning-rate 1e-3 --l2 0.0001 \
+ | tee runs/$RUN/qkeras_version.train.out
 
 # time uv run -m qkeras_version.test \
 #  --fp-int 4 --fp-frac 12 \
@@ -36,25 +35,26 @@ mkdir runs/$RUN || true
 #  --num-train-egs 20000 --epochs 5 --learning-rate 1e-4 --l2 0.0001 \
 #  | tee runs/$RUN/qkeras_version.finetune.out
 
-time uv run -m fxpmath_version.test \
- --min-note A4 --max-note A4 \
- --load-weights runs/$RUN/weights/qkeras/latest.pkl \
- --layer-info runs/$RUN/qkeras_model.layer_info.json \
- --test-x-dir runs/$RUN/test_x_files/ \
- --plot-dir runs/$RUN/ \
- --num-test-egs 500 \
- | tee runs/$RUN/fxpmath_version.test.out
+# time uv run -m fxpmath_version.test \
+#  --min-note A4 --max-note A4 \
+#  --load-weights runs/$RUN/weights/qkeras/latest.pkl \
+#  --layer-info runs/$RUN/qkeras_model.layer_info.json \
+#  --test-x-dir runs/$RUN/test_x_files/ \
+#  --plot-dir runs/$RUN/ \
+#  --num-test-egs 500 \
+#  | tee runs/$RUN/fxpmath_version.test.out
 
 # # quite slow, only required for big changes
 # rm runs/$RUN/test_x_files/zigzag/test_network.y_pred_fxp.pkl || true
 # uv run python -m unittest discover test_equivalences -k test_network
 
 # build & flash
-# pushd /home/mat/dev/tiliqua/gateware
-# rm -rf build/neural-waveshaper-r3/
-# pdm neural_waveshaper build --hw r3 --fs-192khz
-# grep -A30 ^Info:\ Devi build/neural-waveshaper-r3/top.tim
-# openFPGALoader -c dirtyJtag build/neural-waveshaper-r3/top.bit
-# popd
+pushd /home/mat/dev/tiliqua/gateware
+rm -rf build/neural-waveshaper-r3/
+pdm neural_waveshaper build --hw r3 --fs-192khz
+grep -A30 ^Info:\ Devi build/neural-waveshaper-r3/top.tim
+#openFPGALoader -c dirtyJtag build/neural-waveshaper-r3/top.bit || true
+popd
+cp -r /home/mat/dev/tiliqua/gateware/build/neural-waveshaper-r3/ runs/$RUN
 
 # #pdm flash archive build/neural-waveshaper-r3/neural-waveshaper*.tar.gz --slot 1 --noconfirm
