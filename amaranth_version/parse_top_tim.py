dd5e0018dad627c0e81653e0ff6c813ee6d0ab14
Mat Kelcey <matthew.kelcey@gmail.com>
2026-05-24 14:36:48 +1000

    157_4_8_8_relu4; pretrain FP3.15, finetune FP3.6

---
+++ amaranth_version/parse_top_tim.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--top-tim", type=Path, required=True)
+opts = parser.parse_args()
+print("opts", opts)
+
+state = "START"
+
+
+def interesting_du_line(line):
+    for s in ["DP16KD", "MULT18X18D", "TRELLIS_FF", "TRELLIS_COMB"]:
+        if s in line:
+            return True
+    return False
+
+
+last_line = None
+with open(opts.top_tim, "r") as f:
+    for line in f.readlines():
+        line = line.strip()
+
+        if line.startswith("Info: Max frequency for clock"):
+            print(line)
+        elif line.startswith("Info: Router1 time"):
+            print(line)
+        else:
+            match state:
+                case "START":
+                    if line == "Info: Device utilisation:":
+                        state = "DEVICE_UTIL"
+                case "DEVICE_UTIL":
+                    if len(line) == 0:
+                        state = "TIMING"
+                    elif interesting_du_line(line):
+                        print("DU", line)
--- run_end_to_end.sh
+++ run_end_to_end.sh
@@ -6,22 +6,20 @@ export TF_USE_LEGACY_KERAS=1
 export MIN_NOTE=A4
 export MAX_NOTE=A4
 
-export RUN=156_4_8_4_relu4
-export FILTERS="4 8 4"
-
+export RUN=157_4_8_8_relu4
+export FILTERS="4 8 8"
 
 # pre train at FP3.15 ( relu4 )
-# mkdir -p runs/$RUN/pretrain || true
-# time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.train \
-#  --run $RUN/pretrain \
-#  --min-note $MIN_NOTE --max-note $MAX_NOTE --train-interp --soft-clip \
-#  --fp-int 3 --fp-frac 15 \
-#  --filter-sizes $FILTERS --relu-upper-bound 4 \
-#  --alpha-mse 1.0 --beta-stft 0.01 --beta-stft-ramp-epochs 10 \
-#  --num-train-egs 20000 --epochs 10 --learning-rate 1e-3 --l2 0.0001 \
-#  | tee runs/$RUN/pretrain/qkeras_version.train.out
-
-# --harsh-waves --soft-clip \
+date
+mkdir -p runs/$RUN/pretrain || true
+time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.train \
+ --run $RUN/pretrain \
+ --min-note $MIN_NOTE --max-note $MAX_NOTE --train-interp --harsh-waves --soft-clip \
+ --fp-int 3 --fp-frac 15 \
+ --filter-sizes $FILTERS --relu-upper-bound 4 \
+ --alpha-mse 1.0 --beta-stft 0.01 --beta-stft-ramp-epochs 10 \
+ --num-train-egs 50000 --epochs 30 --batch-size 128 --learning-rate 1e-3 --l2 0.0001 \
+ | tee runs/$RUN/pretrain/qkeras_version.train.out
 
 # time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.test \
 #  --fp-int 4 --fp-frac 12 \
@@ -32,16 +30,17 @@ export FILTERS="4 8 4"
 #  | tee runs/$RUN/pretrain/qkeras_version.test.out
 
 # fine tune at FP3.6 ( relu4 )
-# mkdir -p runs/$RUN/fine_tune || true
-# time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.train \
-#  --run ${RUN}/fine_tune \
-#  --min-note $MIN_NOTE --max-note $MAX_NOTE --train-interp --soft-clip \
-#  --fp-int 3 --fp-frac 6 \
-#  --filter-sizes $FILTERS --relu-upper-bound 4 \
-#  --init-weights runs/$RUN/pretrain/weights/keras/ \
-#  --alpha-mse 1.0 --beta-stft 0.01 --beta-stft-ramp-epochs 15 \
-#  --num-train-egs 1000 --epochs 1 --batch-size 128 --learning-rate 1e-5 --l2 0.0001 \
-#  | tee runs/$RUN/fine_tune/qkeras_version.train.out
+date
+mkdir -p runs/$RUN/fine_tune || true
+time uv run --with "tensorflow[and-cuda]==2.16.2" --with "tf_keras" -m qkeras_version.train \
+ --run ${RUN}/fine_tune \
+ --min-note $MIN_NOTE --max-note $MAX_NOTE --train-interp --harsh-waves --soft-clip \
+ --fp-int 3 --fp-frac 6 \
+ --filter-sizes $FILTERS --relu-upper-bound 4 \
+ --init-weights runs/$RUN/pretrain/weights/keras/ \
+ --alpha-mse 1.0 --beta-stft 0.01 --beta-stft-ramp-epochs 0 \
+ --num-train-egs 50000 --epochs 30 --batch-size 128 --learning-rate 1e-5 --l2 0.0001 \
+ | tee runs/$RUN/fine_tune/qkeras_version.train.out
 
 # time uv run -m fxpmath_version.test \
 #  --min-note A4 --max-note A4 \
@@ -57,15 +56,17 @@ export FILTERS="4 8 4"
 # uv run python -m unittest discover test_equivalences -k test_network
 
 # build & flash
+date
 export N_INT=3
-export N_FRAC=15
-export WEIGHTS_PKL=$PWD/runs/$RUN/pretrain/weights/qkeras/latest.pkl
+export N_FRAC=6
+export SUB_RUN=fine_tune
+export WEIGHTS_PKL=$PWD/runs/$RUN/$SUB_RUN/weights/qkeras/latest.pkl
 pushd /home/mat/dev/tiliqua/gateware
 rm -rf build/neural-waveshaper-r3/
 time pdm neural_waveshaper build --hw r3 --fs-192khz
-grep -A30 ^Info:\ Devi build/neural-waveshaper-r3/top.tim
 openFPGALoader -c dirtyJtag build/neural-waveshaper-r3/top.bit || true
 popd
-cp -r /home/mat/dev/tiliqua/gateware/build/neural-waveshaper-r3/ runs/$RUN
+cp -r /home/mat/dev/tiliqua/gateware/build/neural-waveshaper-r3/ runs/$RUN/$SUB_RUN
+uv run -m amaranth_version.parse_top_tim --top-tim runs/$RUN/$SUB_RUN/neural-waveshaper-r3/top.tim
 
 # #pdm flash archive build/neural-waveshaper-r3/neural-waveshaper*.tar.gz --slot 1 --noconfirm
