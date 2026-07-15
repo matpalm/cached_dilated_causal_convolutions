0237a3f6c2593ecfed650cb40622a97799b444aa
Mat Kelcey <matthew.kelcey@gmail.com>
2026-07-15 22:29:59 +1000

    finishing up

--- run_end_to_end.sh
+++ run_end_to_end.sh
@@ -3,47 +3,56 @@ set -ex
 # qkeras 0.9.0 not compatible with keras from in tf 2.16; force legacy package
 export TF_USE_LEGACY_KERAS=1
 
-#export RUN=261_pc600_candidate
-#export RUN=262_pc600_rnd_flip
-#export RUN=263_pc600_rnd_flip_no_skip
-#export RUN=264_pc600_smaller
-#export RUN=265_pc600_smaller  # 8_8_8
-#export RUN=267_pc600_16x4
-#export RUN=268_pc600_16x5
-#export RUN=269_pc600_24x4_ps1
-#export RUN=270_pc006_16x3_quad
-#export RUN=271_pc006_16x3_tri
-#export RUN=272_pc006_16x4_tri
-export RUN=273_pc006_16x4_quad
+#export RUN=275_pc600_16x3_s8_tri
+#export RUN=276_pc600_16x3_s16_tri
+#export RUN=277_pc600_16x3_s128_tri
+#export RUN=278_pc600_16x3_tri_no_stft
+#export RUN=279_pc600_16x3_tri_stft_5_5
+#export RUN=280_pc600_16x3_tri_stft_10_10
+#export RUN=281_pc600_16x3_fixed_lr
+#export RUN=282_pc600_16x3_cosine
+#export RUN=283_pc600_16x3_cosine_quad
+#export RUN=284_pc600_16x3_s8d_cosine_quad
+#export RUN=285_pc600_16x4_cosine_quad
+#export RUN=286_8x3_test
+#export RUN=287_pc600_16x5_cosine_quad
+export RUN=288_pc600_16x5_longer # NEXT ONIGHT
 
-export FILTERS="16 16 16 16"
+export FILTERS="16 16 16 16 16"
 
 export RUN_ID=`echo $RUN | cut -d'_' -f1`
-export PSRAM_ACTIVATION_CACHE_INDICES="[-1]"
-export BUILD=nw_${RUN_ID}_ps1
+export PSRAM_ACTIVATION_CACHE_INDICES="[-3,2,-1]"
+export BUILD=nw_${RUN_ID}_ps123
 export RUN_DIR=$PWD/runs/$RUN/
 export HW=r3
 
 # smoke config
 # export TRAIN_EGS=1_000
 # export PRETRAIN_EPOCHS=10
+# export WARMUP_EPOCHS=1
+# export RAMP_EPOCHS=1
 # export FINETUNE_EPOCHS=5
 
 # sanity config
-export TRAIN_EGS=20_000
-export PRETRAIN_EPOCHS=20
-export FINETUNE_EPOCHS=10
+# export TRAIN_EGS=20_000
+# export PRETRAIN_EPOCHS=20
+# export WARMUP_EPOCHS=5
+# export RAMP_EPOCHS=5
+# export FINETUNE_EPOCHS=5
 
 # onight config
-# export TRAIN_EGS=100_000
-# export PRETRAIN_EPOCHS=100
-# export FINETUNE_EPOCHS=60
+export TRAIN_EGS=100_000
+export PRETRAIN_EPOCHS=100
+export WARMUP_EPOCHS=20
+export RAMP_EPOCHS=20
+export FINETUNE_EPOCHS=20
 
 export SAMPLE_RATE_KHZ=48
-export BATCH_SIZE=32
+export BATCH_SIZE=64
 
 # --skip-project-dim 8 \
 # --quadrature-input
+# --cosine-schedule
 
 pretrain() {
     # pre train at FP3.15 ( relu4 )
@@ -55,9 +64,11 @@ pretrain() {
         --capture-run 600 --keras-model 232_keras/i9 \
         --fp-int 3 --fp-frac 15 --quadrature-input \
         --filter-sizes $FILTERS --relu-upper-bound 4 \
-        --alpha-mse 1.0 --use-huber-loss --beta-stft 0.01 --beta-stft-warmup 5 --beta-stft-ramp 5 \
+        --alpha-mse 1.0 --use-huber-loss --beta-stft 0.001 \
+        --beta-stft-warmup $WARMUP_EPOCHS --beta-stft-ramp $RAMP_EPOCHS \
         --num-train-egs $TRAIN_EGS --epochs $PRETRAIN_EPOCHS --batch-size $BATCH_SIZE \
-        --learning-rate 1e-3 --l2 1e-4 \
+        --learning-rate 1e-3 --cosine-schedule --lr-min-frac 0.01 \
+        --l2 1e-4 \
         | tee runs/$RUN/pretrain/qkeras_version.train.out
 }
 
@@ -70,12 +81,13 @@ finetune() {
         --sample-rate-khz $SAMPLE_RATE_KHZ \
         --train-seq-len-multiplier 2 \
         --capture-run 600 --keras-model 232_keras/i9 \
-        --fp-int $N_INT --fp-frac $N_FRAC \
+        --fp-int $N_INT --fp-frac $N_FRAC --quadrature-input \
         --filter-sizes $FILTERS --relu-upper-bound 4 \
         --init-weights runs/$RUN/pretrain/weights/keras/ \
-        --alpha-mse 1.0 --use-huber-loss --beta-stft 0.001 --beta-stft-warmup 5 --beta-stft-ramp 5 \
+        --alpha-mse 1.0 --use-huber-loss --beta-stft 0.0001 \
+        --beta-stft-warmup 0 --beta-stft-ramp 0 \
         --num-train-egs $TRAIN_EGS --epochs $FINETUNE_EPOCHS --batch-size $BATCH_SIZE \
-        --learning-rate 1e-4 --l2 1e-4 \
+        --learning-rate 1e-4 --cosine-schedule --l2 1e-4 \
         | tee runs/$RUN/finetune_${N_INT}_${N_FRAC}/qkeras_version.train.out
 }
 
@@ -93,11 +105,12 @@ pdm_build() {
       | tee runs/$RUN/$SUB_RUN/parsed_top_tim
 }
 
-pretrain
+#pretrain
 #finetune 3 6
+#finetune 3 7
 #finetune 3 8
 #pdm_build 3 15 pretrain &   # wont' work with psram activation constraints :/
-#pdm_build 3 6 finetune_3_6 &
+pdm_build 3 7 finetune_3_7 &
 #pdm_build 3 8 finetune_3_8 &
 wait
 
